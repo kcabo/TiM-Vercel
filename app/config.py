c1323bff3aa319beb73b8d1e939401946908ce42
kcabo <37576764+kcabo@users.noreply.github.com>
2024-02-25 16:35:45 +0900

    urlstr

--- app/config.py
+++ app/config.py
@@ -2,7 +2,9 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ["POSTGRES_URL"]
+    SQLALCHEMY_DATABASE_URI = os.environ["POSTGRES_URL"].replace(
+        "postgres://", "postgresql://"
+    )
 
     # DBの変更の度にロギングするのを防ぐ
     SQLALCHEMY_TRACK_MODIFICATIONS = False
