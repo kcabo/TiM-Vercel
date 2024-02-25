1a6d0aa5590a4a7690fd5f13501218f8a9f9b867
kcabo <37576764+kcabo@users.noreply.github.com>
2024-02-25 16:30:24 +0900

    env

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
+    SQLALCHEMY_DATABASE_URI = os.environ["POSTGRES_URL"]
 
     # DBの変更の度にロギングするのを防ぐ
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- app/redis_setup.py
+++ app/redis_setup.py
@@ -1,6 +1,6 @@
 import os
 import redis
 
-REDIS_URL = os.environ['REDIS_URL']
+REDIS_URL = os.environ["KV_URL"]
 
 conn = redis.from_url(REDIS_URL, decode_responses=True)
