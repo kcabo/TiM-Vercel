51a75d8d2a396af9aece426835f5ae0cbba1573c
kcabo <37576764+kcabo@users.noreply.github.com>
2024-02-25 22:19:17 +0900

    redis ssl

--- api/_app/redis_setup.py
+++ api/_app/redis_setup.py
@@ -1,6 +1,6 @@
 import os
 import redis
 
-REDIS_URL = os.environ["KV_URL"]
+REDIS_URL = os.environ["KV_URL"].replace("redis://", "rediss://")
 
 conn = redis.from_url(REDIS_URL, decode_responses=True)
