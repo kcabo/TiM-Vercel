c42b56601521e63dcd41364f983bb991c6559833
kcabo <37576764+kcabo@users.noreply.github.com>
2025-04-11 15:28:09 +0900

    💄 メールの本文内で、日付を言及するように変更

--- api/_app/webhook/mailer.py
+++ api/_app/webhook/mailer.py
@@ -14,8 +14,15 @@ def send_mail_with_csv(user, csv: str, target_date: datetime.date):
     subject = target_date.strftime('%m.%d.time')
     from_addr = f'TiM <{BOT_ACCOUNT}>'
     to_addr = user.email
-    bcc_addr = DEVELOPER_ADDRESS
-    body_msg = compose_body_msg(user, target_date)
+    weekdays_ja = ['月', '火', '水', '木', '金', '土', '日']
+    weekday_ja = weekdays_ja[target_date.weekday()]
+    body_msg = f"""
+お疲れ様です。
+{target_date.strftime('%m/%d')}({weekday_ja})の練習のタイムです。
+ご確認よろしくお願いします。
+
+{user.gen}期 {user.name}
+"""
     body = MIMEText(body_msg, 'plain', 'utf-8')
 
     attachment = MIMEText(csv, 'plain', 'utf-8-sig')
@@ -33,26 +40,3 @@ def send_mail_with_csv(user, csv: str, target_date: datetime.date):
     with smtplib.SMTP_SSL(host='smtp.gmail.com', port=465) as smtp:
         smtp.login(BOT_ACCOUNT, APP_PASSWORD)
         smtp.send_message(msg)
-
-
-def compose_body_msg(user, target_date: datetime.date) -> str:
-    today = datetime.date.today()
-    delta = today - target_date
-    gap = delta.days
-
-    if gap == 0:
-        when = '本日'
-    elif gap == 1:
-        when = '昨日'
-    else:
-        when = f'{gap}日前'
-
-    body_msg = f'''
-お疲れ様です。
-{when}の練習のタイムです。
-ご確認よろしくお願いします。
-
-{user.gen}期 {user.name}
-'''
-
-    return body_msg
