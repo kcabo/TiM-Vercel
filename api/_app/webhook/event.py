5048070ebd2afc5aefd2c68e1a825f0f338fbb1e
kcabo <37576764+kcabo@users.noreply.github.com>
2025-04-26 22:35:59 +0900

    💄 ユーザー未登録時のメッセージを変更

--- api/_app/webhook/event.py
+++ api/_app/webhook/event.py
@@ -7,17 +7,17 @@ from ..webhook import dispatcher, humor
 def handle(event_json):
     event = Event(event_json)
 
-    if event.type == "follow":
-        notify(f"{event.line_id}から友達登録されました")
-    elif event.type == "unfollow":
-        notify(f"{event.line_id}からブロックされました")
-    elif event.type not in ["message", "postback"]:
+    if event.type == 'follow':
+        notify(f'{event.line_id}から友達登録されました')
+    elif event.type == 'unfollow':
+        notify(f'{event.line_id}からブロックされました')
+    elif event.type not in ['message', 'postback']:
         return 0
 
     # 操作中のメニューIDを取得
     try:
         event.menu_id = validate_user(event.line_id)
-        if event.type == "message":
+        if event.type == 'message':
             receive_message(event)
         else:
             receive_postback(event)
@@ -25,7 +25,7 @@ def handle(event_json):
     # LINEIDによる検索にヒットしない=未登録
     except UserNotFound:
         # TODO: まだ友達の人に権限を与えるためのフォームを送信する→LIFF
-        msg = "まだユーザー登録されておりません。管理者に登録をお願いしてください。"
+        msg = 'まだユーザー登録されておりません。こちらのフォームからユーザー登録をお願いします。https://forms.gle/cTeuHEqp12AXNTWm8'
         event.send_text(msg)
 
 
@@ -37,31 +37,31 @@ def receive_message(event):
         humor.random_sticker(event)
 
     # メニュー一覧
-    elif text == "一覧":
+    elif text == '一覧':
         today = datetime.date.today()
         dispatcher.view_menus(event, today)
 
     # 操作中のメニューのタイムの確認
-    elif text == "確認":
+    elif text == '確認':
         dispatcher.view_records_scroll(event)
 
     # メールで出力
-    elif text == "メール":
+    elif text == 'メール':
         dispatcher.export_by_mail(event)
 
     # （開発用）LIFFのURLを返す
-    elif text == "テスト":
+    elif text == 'テスト':
         import os
 
-        LIFF_URL = os.environ["LIFF_URL"]
+        LIFF_URL = os.environ['LIFF_URL']
         event.send_text(LIFF_URL)
 
     # タイムの取り込み
-    elif text.find("\n") > 0:
+    elif text.find('\n') > 0:
         dispatcher.add_new_record(event)
 
     # メニュー作成・更新後にタイムを確認するショートカット
-    elif text.startswith("$menu="):
+    elif text.startswith('$menu='):
         menu_id_unsafe = text[6:]
         if menu_id_unsafe.isdecimal():
             menu_id = int(menu_id_unsafe)
@@ -69,7 +69,7 @@ def receive_message(event):
             dispatcher.view_records_scroll(event)
             event.aim_menu_id(menu_id)
         else:
-            event.send_text("？？？？")
+            event.send_text('？？？？')
 
     # 雑談
     else:
@@ -78,53 +78,53 @@ def receive_message(event):
 
 def receive_postback(event):
     try:
-        obj, val = event.postback_data.split("=")
+        obj, val = event.postback_data.split('=')
 
     except ValueError:
-        event.send_text("アンパックできませんでした")
+        event.send_text('アンパックできませんでした')
 
     else:
         # メニュー一覧（三角ボタン押して日付変更した場合）
-        if obj == "date":
-            target_date = datetime.datetime.strptime(val, "%y%m%d")
+        if obj == 'date':
+            target_date = datetime.datetime.strptime(val, '%y%m%d')
             dispatcher.view_menus(event, target_date)
 
         # メニュー一覧（DatePicker使用した場合）
-        elif obj == "picker":
+        elif obj == 'picker':
             val = event.picker_date
-            target_date = datetime.datetime.strptime(val, "%Y-%m-%d")
+            target_date = datetime.datetime.strptime(val, '%Y-%m-%d')
             dispatcher.view_menus(event, target_date)
 
         # メニュー選択
-        elif obj == "menu":
+        elif obj == 'menu':
             menu_id = int(val)
             event.menu_id = menu_id
             dispatcher.view_records_scroll(event)
             event.aim_menu_id(menu_id)
 
         # タイム選択
-        elif obj == "rec":
+        elif obj == 'rec':
             record_id = int(val)
             dispatcher.pick_record(event, record_id)
 
         # レコードの削除
-        elif obj == "delrec":
+        elif obj == 'delrec':
             record_id = int(val)
             dispatcher.delete_record(event, record_id)
 
         # メニュー削除の確認を問う
-        elif obj == "ask":
+        elif obj == 'ask':
             menu_id = int(val)
             dispatcher.ask_whether_delete_menu(event, menu_id)
 
         # メニュー削除の確定
-        elif obj == "delmenu":
+        elif obj == 'delmenu':
             menu_id = int(val)
             dispatcher.delete_menu(event, menu_id)
 
         # メニュー削除操作をキャンセル
-        elif obj == "cancel":
-            event.send_text("やっぱりやめたよ")
+        elif obj == 'cancel':
+            event.send_text('やっぱりやめたよ')
 
         else:
             event.send_text(event.postback_data)
