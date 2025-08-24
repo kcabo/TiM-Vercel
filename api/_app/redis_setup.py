11d4a0ca488a6e7977dd2e86fbe3efebd7ee00a8
kcabo <37576764+kcabo@users.noreply.github.com>
2025-08-24 13:11:07 +0900

    🐛 Redisの接続URLを再設定

--- api/_app/redis_setup.py
+++ api/_app/redis_setup.py
@@ -1,6 +1,6 @@
 import os
 import redis
 
-REDIS_URL = os.environ["KV_URL"].replace("redis://", "rediss://")
+REDIS_URL = os.environ['KV_REDIS_URL']
 
 conn = redis.from_url(REDIS_URL, decode_responses=True)
